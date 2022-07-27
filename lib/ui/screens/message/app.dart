7a18c0c1a8330e3cd979a40ff2e50c6f546f2611
shmsacr <shmsacr@gmail.com>
2022-07-27 17:54:25 +0300

    feat: use streamKey

---
+++ lib/ui/screens/message/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:logger/logger.dart' as log;
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
+
+const streamKey = '9758dfqmq837';
+
+var logger =log.Logger();
+
+/// Extensions can be used to add functionality to the SDK.
+extension StreamChatContext on BuildContext {
+  /// Fetches the current user image.
+  String? get currentUserImage => currentUser!.image;
+
+  /// Fetches the current user.
+  User? get currentUser => StreamChatCore.of(this).currentUser;
+}
