7f1a53a86912868ba89ad1be39439efbd17a20d3
shmsacr <shmsacr@gmail.com>
2022-08-05 11:47:59 +0300

    feat: add export profile

--- lib/ui/screens/screens.dart
+++ lib/ui/screens/screens.dart
@@ -1,7 +1,7 @@
 export 'calendar/calendar.dart';
 export 'home.dart';
 export 'message/message.dart';
-export 'profile.dart';
+export 'profile/profile_screen.dart';
 export 'progress.dart';
 export 'message/chat_screen.dart';
 export 'message/avatar.dart';
