f2c25c72743efe1fdb4facd3084b0f11b4cf223f
shmsacr <shmsacr@gmail.com>
2022-08-10 11:07:41 +0300

    feat: add profile picture change

--- lib/ui/screens/profile/profile_screen.dart
+++ lib/ui/screens/profile/profile_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../controller/auth_controller.dart';
 import '../../theme/app_colors.dart';
@@ -14,66 +19,140 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  late File _uploadFile;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseStorage storage = FirebaseStorage.instance;
+  String? picUrl;
+
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => getImage());
+  }
+
+  getImage() async {
+    String getImageUrl = await storage
+        .ref()
+        .child('profilResimleri')
+        .child(auth.currentUser!.uid)
+        .child('profilResmi.png')
+        .getDownloadURL();
+    setState(() {
+      picUrl = getImageUrl;
+    });
+  }
+
+  Future<void> upLoadCamere() async {
+    XFile? getFile = await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      _uploadFile = File(getFile!.path);
+    });
+    storegeSave();
+  }
+
+  Future<void> upLoadGallery() async {
+    XFile? getFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      _uploadFile = File(getFile!.path);
+    });
+    storegeSave();
+  }
+
+  void storegeSave() async {
+    Reference refPath = storage
+        .ref()
+        .child('profilResimleri')
+        .child(auth.currentUser!.uid)
+        .child('profilResmi.png');
+    UploadTask uploadTask = refPath.putFile(_uploadFile);
+    String url = await (await uploadTask).ref.getDownloadURL();
+    setState(() {
+      picUrl = url;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffdee2fe),
+      backgroundColor: const Color(0xffb7c6d8),
       body: SizedBox(
         width: double.infinity,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Expanded(
-              flex: 4,
-              child: Column(
-                children: [
-                  Container(
-                    height: 120,
-                    width: 120,
-                    margin: const EdgeInsets.only(
-                      top: 100,
-                      bottom: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(2, 2),
-                          blurRadius: 10,
-                        ),
-                      ],
-                      image: const DecorationImage(
-                        image: AssetImage(
-                          "assets/images/dog.jpg",
+            const SizedBox(height: 20),
+            Flexible(
+              flex: 1,
+              child: Container(
+                height: 150,
+                margin: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Stack(children: [
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        margin: const EdgeInsets.all(15),
+                        height: 120,
+                        width: 120,
+                        child: ClipOval(
+                          child: Image.network(
+                            picUrl ??
+                                'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg',
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
+                      Positioned(
+                        bottom: 20,
+                        right: 20,
+                        child: IconButton(
+                            onPressed: () => showDialog(
+                                context: context,
+                                builder: (BuildContext context) =>
+                                    alertDialog()),
+                            icon: const Icon(Icons.add_a_photo)),
+                      )
+                    ]),
+                    SizedBox(
+                      // decoration: const BoxDecoration(
+                      //   color: Colors.red,
+                      // ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Tarik Örnek',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontFamily: 'Montserrat',
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Ders Veren',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black54,
+                                fontFamily: 'Montserrat'),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  const Text(
-                    "Name",
-                    style: TextStyle(
-                      fontFamily: "Montserrat",
-                      color: Colors.black87,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const Text(
-                    "Rol",
-                    style: TextStyle(
-                      fontFamily: "Montserrat",
-                      color: Colors.black54,
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-            Expanded(
-              flex: 5,
+            Flexible(
+              flex: 2,
               child: Container(
+                height: 400,
+                margin: const EdgeInsets.all(20),
                 padding: const EdgeInsets.only(
                   top: 20,
                   left: 24,
@@ -81,9 +160,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
                 decoration: const BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(50),
                   ),
                 ),
                 child: Column(
@@ -98,21 +176,74 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         fontSize: 20,
                       ),
                     ),
-                    const SizedBox(
-                      height: 16,
-                    ),
                     listProfile(Icons.person, "İsim Soyisim", "*****"),
                     listProfile(Icons.male, "Cinsiyet", "****"),
                     listProfile(Icons.mail, 'Mail', '****'),
-                    listProfile(Icons.password, 'Şifre', '****'),
-                    listProfile(Icons.phone, "Telefon Numarası", "****"),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    const _SignOutButton(),
+                    listProfile(Icons.lock, 'Rol', '****'),
                   ],
                 ),
               ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                height: 170,
+                width: double.infinity,
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  left: 24,
+                  right: 24,
+                ),
+                margin: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.key,
+                            color: Colors.black87,
+                          ),
+                          label: const Text(
+                            'Şifre',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.phone_android,
+                            color: Colors.black87,
+                          ),
+                          label: const Text(
+                            '0555 052 20 69',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const _SignOutButton()
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
             ),
           ],
         ),
@@ -120,16 +251,67 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
+  Widget alertDialog() {
+    return AlertDialog(
+      title: const Text('Lütfen yükleme tipini seçiniz'),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: () {
+              upLoadCamere();
+            },
+            child: Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: Icon(
+                    Icons.add_a_photo,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Kamera',
+                  style: TextStyle(color: Colors.purple),
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              upLoadGallery();
+            },
+            child: Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: Icon(
+                    Icons.image,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Galeri',
+                  style: TextStyle(color: Colors.purple),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   Widget listProfile(IconData icon, String text1, String text2) {
     return Container(
       width: double.infinity,
-      margin: const EdgeInsets.only(top: 20),
+      margin: const EdgeInsets.only(top: 40),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Icon(
             icon,
-            size: 20,
+            size: 30,
           ),
           const SizedBox(
             width: 24,
