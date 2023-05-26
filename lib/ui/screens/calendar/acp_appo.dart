edaeea3f4a195cee3d44db3a539abf847eab7e20
shmsacr <shmsacr@gmail.com>
2023-05-26 15:17:41 +0300

    feat: accept appointment screen add features

--- lib/ui/screens/calendar/acp_appo.dart
+++ lib/ui/screens/calendar/acp_appo.dart
@@ -1,6 +1,8 @@
 import 'package:engelsiz/data/models/appointment_with_id.dart';
+import 'package:engelsiz/ui/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
 import '../../../controller/appointment_controller.dart';
 import '../Message/avatar.dart';
@@ -20,25 +22,191 @@ class AcceptAppointment extends ConsumerWidget {
               ),
               body: Column(
                 children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.blueGrey,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(15),
+                  Flexible(
+                    flex: 2,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(top: 60, left: 20, right: 20),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        child: Card(
+                          color: Colors.blueGrey,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Avatar.large(
+                                      url: data["profilePicture"],
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          right: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.15,
+                                          top: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.03),
+                                      child: Text(
+                                        data['fullName'],
+                                        style: const TextStyle(
+                                          color: Colors.black87,
+                                          fontFamily: "Montserrat",
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ),
+                                    // Padding(
+                                    //   padding: EdgeInsets.only(
+                                    //       right: MediaQuery.of(context).size.height *
+                                    //           0.15,
+                                    //       top: MediaQuery.of(context).size.height *
+                                    //           0.03),
+                                    //   child:
+                                    //       Text(appointmentWithId.appointment.title),
+                                    // )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "${DateFormat("yyyy-MM-dd").format(appointmentWithId.appointment.start)}",
+                                          style: const TextStyle(
+                                            color: Colors.black87,
+                                            fontFamily: "Montserrat",
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                        Text(
+                                          "${DateFormat("Hm").format(appointmentWithId.appointment.start)} - ${DateFormat("Hm").format(appointmentWithId.appointment.end)}",
+                                          style: const TextStyle(
+                                            color: Colors.black87,
+                                            fontFamily: "Montserrat",
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          left: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.06),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            appointmentWithId.appointment.title,
+                                            style: const TextStyle(
+                                              color: Colors.black87,
+                                              fontFamily: "Montserrat",
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            appointmentWithId.appointment.notes,
+                                            style: const TextStyle(
+                                                color: Colors.black87,
+                                                fontFamily: "Montserrat",
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 15),
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                    margin: const EdgeInsets.all(50),
-                    child: Row(
-                      children: [
-                        Avatar.large(
-                          url: data["profilePicture"],
+                  ),
+                  Flexible(
+                      flex: 3,
+                      child: Padding(
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.height * 0.09),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            MaterialButton(
+                              color: Colors.redAccent,
+                              onPressed: () async {
+                                try {
+                                  await deleteAppointment(
+                                      appointmentWithId, ref);
+                                  if (context.mounted) {
+                                    Navigator.pop(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                DashboardScreen()));
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text("Randevu reddeildi")),
+                                    );
+                                  }
+                                } catch (e) {
+                                  debugPrint(e.toString());
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(content: Text("$e")));
+                                }
+                              },
+                              child: const Text(
+                                "Reddet",
+                              ),
+                            ),
+                            MaterialButton(
+                              color: Colors.greenAccent,
+                              onPressed: () async {
+                                try {
+                                  await updateAppointment(
+                                      appointmentWithId.id, ref);
+                                  if (context.mounted) {
+                                    Navigator.pop(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                DashboardScreen()));
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                          content: Text("Randevu onaylandı")),
+                                    );
+                                  }
+                                } catch (e) {
+                                  debugPrint(e.toString());
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(content: Text("$e")));
+                                }
+                              },
+                              child: const Text(
+                                "Kabul et",
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          data['fullName'],
-                        ),
-                      ],
-                    ),
-                  )
+                      ))
                 ],
               ),
             ),
