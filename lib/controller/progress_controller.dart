75649298987a91210cdbcf546e049f5e886b1691
seheerst <seherst9@gmail.com>
2023-05-26 21:40:42 +0300

    feat: changed uploadPDFToFirebase method

--- lib/controller/progress_controller.dart
+++ lib/controller/progress_controller.dart
@@ -1,36 +1,46 @@
 import 'dart:io';
-import 'dart:ui';
-import 'package:path/path.dart' as path;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
-import '../ui/screens/progress/filePreview.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:firebase_core/firebase_core.dart';
 
-FirebaseAuth auth = FirebaseAuth.instance;
-FirebaseStorage storage = FirebaseStorage.instance;
-FirebaseFirestore firestore = FirebaseFirestore.instance;
-late final Channel channel;
-Future<String> uploadPDFToFirabase(File pdfFile) async{
+import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart'
+as stream_chat;
+import 'auth_controller.dart';
+
+Future<String> uploadPDFToFirebase(File pdfFile,stream_chat.Channel channel,
+    stream_chat.User currentUser, WidgetRef ref) async {
+  final otherMembers = channel.state?.members.where(
+        (element) => element.userId != currentUser.id,
+  );
+  final parentId = otherMembers!.first.userId;
   String pdfName = path.basename(pdfFile.path);
-  Reference ref = storage.ref().child('progressMedia').child(auth.currentUser!.uid).child(pdfName);
+  Reference ref = FirebaseStorage.instance.ref()
+      .child('progressMedia')
+      .child(parentId!)
+      .child(pdfName);
   UploadTask uploadTask = ref.putFile(pdfFile);
   TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
   String downloadUrl = await taskSnapshot.ref.getDownloadURL();
   return downloadUrl;
 }
 
-Future<void> uploadPDF(BuildContext context) async{
+Future<void> uploadPDF(stream_chat.Channel channel,
+    stream_chat.User currentUser, WidgetRef ref) async {
   FilePickerResult? result = await FilePicker.platform.pickFiles(
     type: FileType.custom,
     allowedExtensions: ['pdf'],
   );
-  if(result != null){
-    File pdfFile = File(result.files.single.path ?? '');
-    String pdfUrl = await uploadPDFToFirabase(pdfFile);
-
+  if (result != null) {
+    File pdfFile = File(result.files.single.path!);
+    String pdfUrl = await uploadPDFToFirebase(pdfFile, channel, currentUser, ref);
+    print('PDF download URL: $pdfUrl');
   }
 }
+
+
 
